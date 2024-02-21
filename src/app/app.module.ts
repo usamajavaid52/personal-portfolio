595a8ecf6d4ff0b6833f8201540de6a5ff33e972
Usama Javaid <usamajavaid52@gmail.com>
2024-02-21 20:37:02 +0500

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxTypedJsModule } from 'ngx-typed-js';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgxTypedJsModule],
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, NgxTypedJsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
