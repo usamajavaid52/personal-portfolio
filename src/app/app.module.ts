59bb65e17d5f4fdf059d3889ce418dbe742108f0
Usama Javaid <usamajavaid52@gmail.com>
2024-11-16 15:32:49 +0500

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,4 +13,4 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
