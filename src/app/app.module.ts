ee6e4a009bd7f073c36c9228e75e45e7b9ad5262
Usama Javaid <usamajavaid52@gmail.com>
2025-03-04 12:42:00 +0500

    updated navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { NgxTypedJsModule } from 'ngx-typed-js';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, NgxTypedJsModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, NgxTypedJsModule, BrowserAnimationsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent implements OnInit {
-  constructor() { }
+export class NavbarComponent {
 
-  ngOnInit(): void { }
+  activeSection: string = 'about'; // Default active section
+  isMenuOpen: Boolean = false
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    // Logic to determine active section based on scroll position
+    const aboutSection = document.getElementById('about');
+    const experiencesSection = document.getElementById('experiences');
+    const educationSection = document.getElementById('education');
+    const skillsSection = document.getElementById('skills');
+
+    if (aboutSection && window.scrollY >= aboutSection.offsetTop - 100) {
+      this.activeSection = 'about';
+    }
+    if (experiencesSection && window.scrollY >= experiencesSection.offsetTop - 100) {
+      this.activeSection = 'experiences';
+    }
+    if (educationSection && window.scrollY >= educationSection.offsetTop - 100) {
+      this.activeSection = 'education';
+    }
+    if (skillsSection && window.scrollY >= skillsSection.offsetTop - 100) {
+      this.activeSection = 'skills';
+    }
+  }
+
+  setActive(section: string) {
+    this.activeSection = section;
+    this.closeMenu(); // Close the menu when a link is clicked
+  }
+
+
+
+  closeMenu() {
+    console.log("ye b chal rha ha ?")
+    if (window.innerWidth <= 890) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  downloadPDF() {
+    const pdfUrl = 'assets/usamajavaid.pdf'; // Replace with your actual PDF file path
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.setAttribute('download', 'usama_cv.pdf'); // Set the default file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 }
